1117d17e318f002e32c3698e0594f6cec36e7501
bahaaCs <bahaaaboukhalil@gmail.com>
2024-10-27 16:19:29 +0200

    Ex1.py: built the diamond shape

--- Ex1.py
+++ Ex1.py
@@ -1,20 +1,22 @@
-x=4
+x=21
 
 stars_n=1
 
 for i in range(1,x+1):
     spacing_n=int((x-i))
     spacing=" "*spacing_n
+    
     output="*"*stars_n
     print(spacing+output)
-    stars_n+=2
-
-starts_n=x
+    stars_n=stars_n+2 if i<x else stars_n
+    
 for i in range(x,1,-1):
-    spacing_n=int((x-i))
+    stars_n-=2
+    
+    spacing_n=int((x-i+1))
     spacing=" "*spacing_n
     
     output="*"*stars_n
     print(spacing+output)
-    stars_n-=2
     
+
