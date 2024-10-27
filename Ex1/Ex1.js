234b1782cb92f4cf7fc5a54d4c8f05809d61049c
bahaaCs <bahaaaboukhalil@gmail.com>
2024-10-27 17:52:50 +0200

    Ex1.js: built the other half of diamond shape

--- Ex1/Ex1.js
+++ Ex1/Ex1.js
@@ -12,3 +12,15 @@ for(let i=1;i<=rows;i++){
     if(i<rows)
         stars_n=stars_n+2
 }
+
+for(let i=rows;i>0;i--){
+
+    stars_n-=2
+
+    var spacing_n=rows-i+1
+    var spacing = " ".repeat(spacing_n)
+
+    var output = "*".repeat(stars_n)
+    console.log(spacing+output)
+
+}
