7cdcc6120376a2047fdbbe8f0edf8766e9f25ae6
bahaaCs <bahaaaboukhalil@gmail.com>
2024-10-27 16:01:29 +0200

    Ex1.py: building the other half of diamond shape

--- Ex1.py
+++ Ex1.py
@@ -1,10 +1,20 @@
-x=11
+x=4
+
 stars_n=1
 
 for i in range(1,x+1):
-    j=int((x-i))
-    spacing=" "*j
+    spacing_n=int((x-i))
+    spacing=" "*spacing_n
     output="*"*stars_n
     print(spacing+output)
     stars_n+=2
+
+starts_n=x
+for i in range(x,1,-1):
+    spacing_n=int((x-i))
+    spacing=" "*spacing_n
+    
+    output="*"*stars_n
+    print(spacing+output)
+    stars_n-=2
     
