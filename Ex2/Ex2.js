139415981917d2ba0df404eacb81aa7d63536fd4
bahaaCs <bahaaaboukhalil@gmail.com>
2024-10-27 22:12:55 +0200

    Ex2: edit Ex2.js logic

--- Ex2/Ex2.js
+++ Ex2/Ex2.js
@@ -1,9 +1,9 @@
 var x = prompt("Enter x: ")
 
-var max_odd_numbers = 1
+var odd_number = 1
 var odd_numbers = []
-while (max_odd_numbers<=x){
-    odd_numbers.push(String(max_odd_numbers))
+for(let i=1;i<x;i++){
+    odd_numbers.push(String(odd_number))
     console.log(odd_numbers.join(" "))
-    max_odd_numbers+=2
+    odd_number+=2
 }
