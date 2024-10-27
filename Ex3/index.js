09db8d73e86a768e86000d422e8c30097fb1a37f
bahaaCs <bahaaaboukhalil@gmail.com>
2024-10-27 20:51:40 +0200

    Ex3: Trying to add tree into my div

--- Ex3/index.js
+++ Ex3/index.js
@@ -1,4 +1,4 @@
-document.querySelector("body").appendChild(document.createElement("div"))
+
 
 var x = prompt("Number of rows: ")
 var stars_n = 1
@@ -6,13 +6,25 @@ var spacing_n
 var spacing
 var output
 
+var tree=[]
+
 for(let i=1;i<=x;i++){
     spacing_n=x-i
     spacing = " ".repeat(spacing_n)
 
     output = "*".repeat(stars_n)
-    console.log(spacing+output)
+    
+    tree.push(spacing+output)
 
     if(i<x)
         stars_n=stars_n+2
 }
+
+var html_content=tree.join('\n')
+console.log(html_content)
+
+var tree_div = document.createElement("div")
+tree_div.classList.add("tree")
+tree_div.textContent=html_content
+document.body.appendChild(tree_div)
+
